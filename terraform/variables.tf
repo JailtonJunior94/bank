2276a2a02d61e3dee408d155463bb337efc71f80
jailton.junior <jailton.junior@iterative.com.br>
2021-08-20 18:05:43 -0300

    Atualização de manisfest k8s

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "do_token" {
   type        = string
   description = "Token do provedor Digital Ocean"
-  default     = "034bdfb3309f2f5d39e67ca941d4566b3734648c381de0bd79369ea569836cb6"
+  default     = ""
 }
 
 variable "region" {
